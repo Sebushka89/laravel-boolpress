e17236ea18c597b7a44070cfa0e581bb4d1c50d8
Sebushka89 <sebastian.rizzo@hotmail.it>
2021-09-17 18:02:34 +0200

    created the modal for delete the post

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -25,42 +25,44 @@
                 <td>{{$post->author}}</td>
                 <td><img src="{{$post->cover}}" alt="picture of {{$post->author}}" /></td>
                 <!-- a href="/posts/{{$post->id}}" -->
-                <td class="d-flex">
-                    <a href="{{ route('posts.show', $post) }}">
-                        <button class="btn btn-primary">
-                            <i class="bi bi-zoom-in"></i>
-                        </button>
-                    </a>
-                    <a href="{{ route('posts.edit', $post) }}">
-                        <button class="btn btn-warning">
-                            <i class="bi bi-arrow-repeat"></i>
-                        </button>
-                    </a>
-                    <button type="submit" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal"><i class="bi bi-trash"></i></button>
-                    
-                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                      <div class="modal-dialog modal-dialog-centered" role="document">
-                        <div class="modal-content">
-                          <div class="modal-header bg-danger">
-                            <h5 class="modal-title" id="exampleModalLabel">ATTENZIONE</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                              <span aria-hidden="true">&times;</span>
+                <td>
+                    <div class="d-flex align-items-center justify-content-end">
+                        <a href="{{ route('posts.show', $post) }}">
+                            <button class="btn btn-primary">
+                                <i class="bi bi-zoom-in"></i>
                             </button>
-                          </div>
-                          <div class="modal-body">
-                            Sei veramente sicuro di voler eliminare l'elemento?
-                          </div>
-                          <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Torna indietro</button>
-                            <form action="{{route('posts.destroy', $post)}}" method="POST">
-                              @csrf
-                              @method('DELETE')
-                              <button type="submit" data-target="#exampleModal" class="btn btn-danger">Elimina</button>
-                            </form>
+                        </a>
+                        <a href="{{ route('posts.edit', $post) }}">
+                            <button class="btn btn-warning">
+                                <i class="bi bi-arrow-repeat"></i>
+                            </button>
+                        </a>
+                        <button type="submit" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal"><i class="bi bi-trash"></i></button>
+                        
+                        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered" role="document">
+                            <div class="modal-content">
+                            <div class="modal-header bg-danger">
+                                <h5 class="modal-title" id="exampleModalLabel">ATTENZIONE</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                                </button>
                             </div>
-                          </div>
+                            <div class="modal-body">
+                                Sei veramente sicuro di voler eliminare l'elemento?
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Torna indietro</button>
+                                <form action="{{route('posts.destroy', $post)}}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" data-target="#exampleModal" class="btn btn-danger">Elimina</button>
+                                </form>
+                                </div>
+                            </div>
+                            </div>
                         </div>
-                      </div>
+                    </div>
                 </td>
             </tr>
         @endforeach
