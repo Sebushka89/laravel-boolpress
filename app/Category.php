a946afaf715395997475bf08332d2b3ae69b2e1f
Sebushka89 <sebastian.rizzo@hotmail.it>
2021-09-22 15:20:04 +0200

    error seed

--- app/Category.php
+++ app/Category.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     public function post() {
-        return $this->hasMany(Book::class);
+        return $this->hasMany(post::class);
     }
 }
