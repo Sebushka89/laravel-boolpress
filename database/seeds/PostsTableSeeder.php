3dc2aade255e186d6ea9d17a9da93a6fa3468f7d
Sebushka89 <sebastian.rizzo@hotmail.it>
2021-09-22 15:17:12 +0200

    error seed

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 
 use App\post;
+use App\Category;
 
 use Faker\Generator as Faker;
 
@@ -15,13 +16,35 @@ class PostsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        $categoryList = [
+            'Cronaca',
+            'Sport',
+            'Opinione',
+            'Turismo',
+            'Musica',
+            'Cinema'
+        ];
+
+        $listOfCategoryID = []; 
+
+        foreach($categoryList as $category) {
+            $categoryObject = new Category();
+            $categoryObject->name = $category;
+            $categoryObject->save();
+            $listOfCategoryID[] = $categoryObject->id;
+        }
+
         for ($i = 0; $i < 50; $i++){
 
+            
             $postObject = new post();
             $postObject->data = $faker->dateTime(); 
             $postObject->title = $faker->sentence(5);  
             $postObject->author = $faker->sentence(2); 
             $postObject->cover = $faker->imageUrl(640, 480, 'posts', true);
+            $randCategoryKey = array_rand($listOfCategoryID, 1);
+            $categoryID = $listOfCategoryID[$randCategoryKey];
+            $postObject->category_id = $categoryID;
             $postObject->save();
         }
     }
